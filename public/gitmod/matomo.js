bfcbd6367b36c9d3c2cb140417f8fd2c62a665ab
Matthew Grasmick <matthew.grasmick@acquia.com>
2022-02-04 10:26:03 -0500

    Update matomo.js

--- public/gitmod/matomo.js
+++ public/gitmod/matomo.js
@@ -11,3 +11,8 @@ if (monitorLink = document.querySelector('[data-track-label="monitor_menu"]')) {
     monitorLink.style.display = "none";
 }
 document.evaluate("//a[contains(text(),'Add Kubernetes cluster')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.display = "none";
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-IJB0Z39VSYPZ-2-2");
